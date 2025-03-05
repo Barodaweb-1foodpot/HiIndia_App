c42cb4c6320f7ffcfea21a5c961c0c2fad40914f
Deep1012 <deep.inbweb25@gmail.com>
2025-03-05 15:26:07 +0530

    updates

--- App.js
+++ App.js
@@ -4,13 +4,12 @@ import Toast from "react-native-toast-message";
 import MainNavigator from "./src/navigation/MainNavigator";
 import ContextProvider from "./src/context/AuthContext";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import { STRIPE_PUBLISHABLE_KEY } from "@env";
 
 const App = () => {
   return (
     <ContextProvider>
-      <StripeProvider
-        publishableKey="pk_test_51Qyng1FsN0sGP8PLbYXNlyWfmu3VHrkA8yt5GahyJyqzpCW1mbiyZtYwKi4wIsjUmE2Qc9lud4zFyvZ94YS4iDTh00Siqmhzp0"
-      >
+      <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
       <NavigationContainer>
         <MainNavigator />
         <Toast />
