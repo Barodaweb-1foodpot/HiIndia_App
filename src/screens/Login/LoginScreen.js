b05ddbdcc8e61c53cf2b5e378d2a878b3808f0d0
Deep1012 <deep.inbweb25@gmail.com>
2025-03-20 13:14:48 +0530

    Login google commented

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -180,7 +180,7 @@ const LoginScreen = ({ navigation }) => {
                         </TouchableOpacity>
                       </View>
 
-                       <View style={styles.dividerContainer}>
+                       {/* <View style={styles.dividerContainer}>
                         <View style={styles.divider} />
                         <Text style={styles.dividerText}>or</Text>
                         <View style={styles.divider} />
@@ -197,7 +197,7 @@ const LoginScreen = ({ navigation }) => {
                         <Text style={styles.socialButtonText}>
                           Continue with Google
                         </Text>
-                      </TouchableOpacity> 
+                      </TouchableOpacity>  */}
 
                       <View style={styles.termsContainer}>
                         <Text style={styles.termsText}>
