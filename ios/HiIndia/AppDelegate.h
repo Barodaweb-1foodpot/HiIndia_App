19fc514cb648dc6881578f406f492a809b17882d
TheVegeta <iamvegeta@protonmail.com>
2025-04-03 12:05:41 +0530

    ios

--- ios/HiIndia/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-#import <RCTAppDelegate.h>
-#import <UIKit/UIKit.h>
-#import <Expo/Expo.h>
-#import <UserNotifications/UserNotifications.h>
-
-@interface AppDelegate : EXAppDelegateWrapper <UNUserNotificationCenterDelegate>
-
-@end
